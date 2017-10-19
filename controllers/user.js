eeefb0d066eeed30236ac0e0b0758c46b5e98240
许帅 <xushuai.ceo@gmail.com>
2017-10-19 19:15:21 +0800

    注册登录接口

--- controllers/user.js
+++ controllers/user.js
@@ -1,33 +1,92 @@
 const query = require('../unit/query');
 
 exports.getUser = async (ctx)=>{
-    ctx.body = '你好啊！ Koa2.';
-    query("select * from users", function(err, vals, fields){
-        if(err){
-            console.log(err);
-        }else{
-            console.log(vals);
-        }
-    })
+    ctx.body = '你好啊！ Koa2.' + 'header0'+Math.floor(Math.random()*4+1)+'.png';
+};
+
+exports.loginUser = async (ctx)=>{
+
+    let response = {
+        success: false,
+        info: '',
+        userinfo: null,
+    };
+    response.userinfo = JSON.parse(ctx.request.body);
+    console.log(response.userinfo);
+
+    let getUserQuery = `select * from slf_user where tel='${response.userinfo.tel}' and password='${response.userinfo.password}'`;
+
+    function save(){
+        return new Promise((reslove, reject)=>{
+            query(getUserQuery, function(err, vals){
+                if(err){
+                    reject(err);
+                }else{
+                    if(vals.length>0){
+                        response.success = true;
+                        response.info = '登录成功！';
+                        response.userinfo = vals[0];
+                        reslove(response);
+                    }else{
+                        response.info = '用户名或密码不正确！';
+                        reslove(response);
+                    }
+                }
+            });
+        })
+    }
+
+    await save();
+
+    console.log(response);
+    ctx.body = response;
+
 };
 
 exports.registerUser = async (ctx)=>{
-    let userinfo = JSON.parse(ctx.request.body);
-    let queryString = `insert into users(username,tel,password) values ('${userinfo.tel}','${userinfo.tel}','${userinfo.password}')`;
-    query(queryString, function(err, vals){
-        if(err){
-            ctx.body = {
-                success: false,
-                info: '该手机号已注册！'
-            };
-            throw err;
-        }
-    })
 
-    ctx.body = {
-        success: true,
-        info: '注册成功！',
-        userinfo: userinfo
+    let response = {
+        success: false,
+        info: '',
+        userinfo: null,
     };
+    response.userinfo = JSON.parse(ctx.request.body);
+    response.userinfo.username = '福民_'+response.userinfo.tel.substr(5);
+    response.userinfo.regTime = new Date();
+    response.userinfo.avatar = 'header0'+Math.floor(Math.random()*4+1)+'.png';
+    console.log(response.userinfo.avatar);
+
+    let getUserQuery = `select * from slf_user where tel='${response.userinfo.tel}'`
+    let addUserQuery = `insert into slf_user(username,tel,password,regtime,avatar) values ('${response.userinfo.username}','${response.userinfo.tel}','${response.userinfo.password}','${response.userinfo.regTime}','${response.userinfo.avatar}')`;
+
+    function save(){
+        return new Promise((reslove, reject)=>{
+            query(getUserQuery, function(err, vals){
+                if(err){
+                    reject(err);
+                }else{
+                    if(vals.length>0){
+                        response.info = '当前手机号已注册';
+                        reslove(response);
+                    }else{
+                        query(addUserQuery, function(err){
+                            if(err){
+                                reject(err);
+                            }else{
+                                response.success = true;
+                                response.info = '注册成功';
+                                reslove(response);
+                            }
+                        });
+                    }
+                }
+            });
+        })
+    }
+
+    await save();
+
+    console.log(response);
+    ctx.body = response;
 
 };
--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,9 @@ router.prefix('/api/users');
 
 router.get('/', user.getUser);
 
+//用户注册
 router.post('/register', user.registerUser);
+//用户登录
+router.post('/login', user.loginUser);
 
 module.exports = router;
